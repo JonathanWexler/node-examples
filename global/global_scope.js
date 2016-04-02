37fd1628dc96ee5e69c9977ddd9a1b988a10a28d
Jon Wexler <Jon@Instructors-MacBook-Pro.local>
2016-04-02 14:09:47 -0400

    string interpolation

---
+++ global/global_scope.js
@@ -0,0 +1,14 @@
+// Creating a variable will not make it global with node, like it will in a browser console
+var word = "Hello There!";
+
+// In terminal this should give us undefined
+console.log(global.word);
+
+// If we remove the global namespace we can access the variable
+console.log(word);
+
+// To get a substring from the string we can use Slice()
+var sub = word.slice(5);
+
+// We can use the backticks to interpolate the string with a variable
+console.log(`The second word is ${sub}`);
