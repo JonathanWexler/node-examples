8ec9f50673156775a6c7241e692c974b0413af2e
Jon Wexler <Jon@Instructors-MacBook-Pro.local>
2016-04-02 12:47:15 -0400

    Node examples init

---
+++ global/basic_global.js
@@ -0,0 +1,6 @@
+// This will print 'Hello World!' when run with node basic_global.js
+console.log("Hello World!");
+
+// This will print 'Hello World!' when run with node basic_global.js
+// Here we are specifying the global namespace, which is assumed normally
+global.console.log("Hello World!");
