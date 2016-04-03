76bce0a6074dbece69d0487edd5675fd4e144fdb
Jonathan Wexler <jwexman@gmail.com>
2016-04-02 21:15:17 -0400

    complete input and output

--- process/input_output.js
+++ process/input_output.js
@@ -9,3 +9,32 @@ var questions = [
 	"What is your favorite sport?"
 ];
 
+// Now we can set up a function to ask questions to the user via terminal
+function ask (q) {
+	process.stdout.write(`${questions[q]}\n`);
+	process.stdout.write(">");
+};
+
+// We can set up an array of answers to keep track of user input
+var answers = [];
+
+// This code block will listen for data being sent to the applciation and echo back that data
+process.stdin.on('data', function(data){
+	// process.stdout.write(`\n${data.toString().trim()}\n`);
+
+	answers.push(data.toString().trim());
+
+// After adding the response to the answers array we check if there are more questions to ask and ask them
+	if (answers.length < questions.length) {
+		ask(answers.length);
+	} else {
+		process.exit();
+	};
+});
+
+
+process.on('exit', function(){
+	process.stdout.write(`Hello ${answers[1]}, you are ${answers[0]} and love ${answers[2]}\n`);
+})
+
+ask(0);
