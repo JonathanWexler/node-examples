7cf60e6ec97c6abe288c75c14c0f90ac3c161cc8
Jonathan Wexler <jwexman@gmail.com>
2016-04-02 20:54:26 -0400

    basic output

---
+++ process/input_output.js
@@ -0,0 +1,11 @@
+// We can write to the console with stdout.write
+process.stdout.write('Hello');
+process.stdout.write('World \n\n\n!');
+
+// We can add more information in the form of variables
+var questions = [
+	"How old are you?",
+	"What is your name?",
+	"What is your favorite sport?"
+];
+
