601c6e4a58840c42cc2cdac695e756bbc773d492
Jonathan Wexler <jwexman@gmail.com>
2016-04-14 02:49:00 -0400

    async readdir

--- 4_core_modules/7_read_files.js
+++ 4_core_modules/7_read_files.js
@@ -5,3 +5,15 @@ var fs = require('fs');
 var files = fs.readdirSync('./4_modules')
 // Above will read the files from the specified directory synchronously, and return those values we can print
 console.log(files);
+
+// To read asynchronously we remove 'Sync', but we need a callback function when complete
+// The callback will take a potential error and read files/data
+fs.readdir('./4_modules', function(error, data){
+	if (error) {
+		throw error;
+	};
+	// print files
+	console.log(data);
+});
+
+console.log("Will run first due to asynchronous call above:");
