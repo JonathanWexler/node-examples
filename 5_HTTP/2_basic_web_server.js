3315f8008c9bbf48f2039f35f581d414c79acf6b
Jonathan Wexler <jwexman@gmail.com>
2016-04-25 05:56:35 -0400

    mini web server beginning

--- 5_HTTP/2_basic_web_server.js
+++ 5_HTTP/2_basic_web_server.js
@@ -7,9 +7,23 @@ var port = 3000;
 // Then to create the server we use the createServer method which takes a callback function with the request and response
 var server = http.createServer(function(req, res){
 	// We can setup the header for the response here with status code and content type
-	res.writeHead(200, {'Content-Type':'text/plain'});
+	res.writeHead(200, {'Content-Type':'text/html'});
 	// We can also print a message when the server delivers a response
-	res.end("Request received!")
+	res.end(`
+			<!DOCTYPE html>
+			<html>
+				<head>
+					<title> Basic Server</title>
+				</head>
+				<body>
+					<h1> Request Received! </h1>
+					<h3>URL: ${req.url}</h3>
+					<h3>METHOD: ${req.method}</h3>
+				</body>
+			</html>
+
+
+		`)
 })
 
 // Now we need to listen for requests to our running server, indicating whatever port we want
